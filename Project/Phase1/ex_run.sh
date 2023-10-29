d024a2fb0b6941f041bd51e97314be0e5e7b00a6
qwertyuiop41 <3111205688@qq.com>
2023-10-29 14:54:37 +0800

    update .sh

--- Project/Phase1/ex_run.sh
+++ Project/Phase1/ex_run.sh
@@ -1,2 +1,2 @@
-./bin/splc test_ex/test_1.spl > output_ex/output1.txt
-./bin/splc test_ex/test_2.spl > output_ex/output2.txt
+bin/splc test_ex/test_1.spl > output_ex/output1.txt
+bin/splc test_ex/test_2.spl > output_ex/output2.txt
